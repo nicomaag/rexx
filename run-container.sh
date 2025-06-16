cb61f768e6dc65af90653a235c4413ddae6ab34f
nicomaag <nico.maag@web.de>
2025-06-16 18:42:16 +0200

    update scripts

--- run-container.sh
+++ run-container.sh
@@ -3,8 +3,9 @@
 
 IMAGE="nicomaa/rexx:latest"
 LOG_DIR="/data/rexx/logs" # Change this to your desired log directory
+ENV_FILE_DIR="/data/rexx/.env" # Change this to your desired env file directory
 mkdir -p "$LOG_DIR"
 LOG_FILE="$LOG_DIR/rexx-$(date +"%Y-%m-%d_%H-%M-%S").log"
 
 docker pull "$IMAGE"
-docker run --rm "$IMAGE" > "$LOG_FILE" 2>&1
+docker run --rm --env-file "$ENV_FILE_DIR" "$IMAGE" > "$LOG_FILE" 2>&1
